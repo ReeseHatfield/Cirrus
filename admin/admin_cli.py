59686ed4b297189fb569a10a208da655b5ae7f17
Reese Hatfield <hatfield.69@wright.edu>
2023-12-28 21:07:42 -0500

    add basic CLI

--- admin/admin_cli.py
+++ admin/admin_cli.py
@@ -1,4 +1,41 @@
-print("ADMIN:")
-s = input("Input test")
+def list_users():
+    print("listing users...")
 
-print(f"You entered {s}")
+def add_user():
+    print("adding user")
+
+def remove_user():
+    print("removiing user")
+
+func_table = {
+    1: list_users,
+    2: add_user,
+    3: remove_user,
+    9: lambda : exit(0)
+}
+
+
+def print_menu():
+
+    print("1: List Users")
+    print("2: Add User")
+    print("3: Remove User")
+    print("9: Exit")
+
+def main():
+    print("Welcome to the Cirrus Administrator Console!")
+
+    while(True):
+        print_menu()
+        
+        try:
+            choice = int(input())
+            func_table[choice]()
+        except (KeyError, ValueError):
+            print("Invalid input")
+
+
+
+
+if __name__ == "__main__":
+    main()
