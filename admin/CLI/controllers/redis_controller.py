4e654ddb6d1d170718ac19d0819e4296282533d4
Reese Hatfield <hatfield.69@wright.edu>
2024-01-02 15:11:51 -0500

    relative import fix
    Also tests get_instance()

--- admin/CLI/controllers/redis_controller.py
+++ admin/CLI/controllers/redis_controller.py
@@ -1,13 +1,18 @@
-# controller layer should handle input validation, error handing, and checks if user exists
+from CLI.models.redis_client import Redis_Client
 
+# controller layer should handle input validation, error handing, and checks if user exists
 
 def add_user(user_name: str, pass_word: str):
     print(user_name)
     print(pass_word)
     print("add user from controller")
 
+    client = Redis_Client.get_instance()
+
+
 def get_users():
-    pass
+    client = Redis_Client.get_instance()
+
 
 def delete_user(user_name: str):
-    pass
+    client = Redis_Client.get_instance()
