3b7b30579a66d202be1ac16e62426af6de688454
Reese Hatfield <hatfield.69@wright.edu>
2024-01-03 10:00:45 -0500

    Backend now auths with redis server

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,41 +3,82 @@ import StatusCodes from "../config/StatusCodes";
 import hash from "../security/hasher"
 const fs = require('fs');
 import crypto from 'crypto';
-import { error } from "console";
+import { createClient } from "redis";
 
-export const authenticateUser = (req: Request, res: Response) => {
+export const authenticateUser = async (req: Request, res: Response) => {
+    const client = await createClient({
+        url: 'redis://host.docker.internal:6379'
+    }).on('error', err => console.log("Redis Client Error")).connect();
+
+
     // TODO: move into model
     try{
         const { username, password } = req.body;
 
-        const dbPath = "../backend/src/security/database.json";
-        let database = fs.readFileSync(dbPath, "utf-8");
+        console.log(`Username: ${username}, Password: ${password}`);
 
-        let users: any = JSON.parse(database);
-        const index = users.findIndex((user: any) => user[username]);
+        // const dbPath = "../backend/src/security/database.json";
+        // let database = fs.readFileSync(dbPath, "utf-8");
 
-        if (index === -1) {
+        // let users: any = JSON.parse(database);
+        // const index = users.findIndex((user: any) => user[username]);
+
+        // if (index === -1) {
+        //     console.log("User not found.");
+        //     res.status(StatusCodes.UNAUTHORIZED).json({
+        //         message: "User Unauthorized"
+        //     })
+        //     return;
+        // }
+
+
+        const value: string | null = await client.get(username);
+
+        if(value == null){
             console.log("User not found.");
             res.status(StatusCodes.UNAUTHORIZED).json({
                 message: "User Unauthorized"
             })
             return;
         }
-        
-        const userData = users[Object.keys(users)[index]];
 
-        //really need to fix this line at some point
-        const salt = JSON.parse(JSON.stringify(Object.values(userData)[0])).salt;
-        const correctHashedPassword = JSON.parse(JSON.stringify(Object.values(userData)[0])).password;
+        const [correctHash, salt ]= value.split(',');
+
+        console.log(correctHash);
+        console.log(salt);
 
         const tryHash = salt + password;
-
+        
         const hashed: string = crypto.createHash('sha256').update(tryHash).digest('hex');
+
+
+        // if(correctHashedPassword === hashed){
+        //     res.status(StatusCodes.OK).json({
+        //         message: "User authenticated",
+        //         sessionID: sessionID
+        //     });
+        // }
+        // else{
+        //     res.status(StatusCodes.UNAUTHORIZED).json({
+        //         message: "User Unauthorized"
+        //     })
+        // }
+
+
+        //const userData = users[Object.keys(users)[index]];
+
+        //really need to fix this line at some point
+        // const salt = JSON.parse(JSON.stringify(Object.values(userData)[0])).salt;
+        // const correctHashedPassword = JSON.parse(JSON.stringify(Object.values(userData)[0])).password;
+
+        // const tryHash = salt + password;
+
+        // const hashed: string = crypto.createHash('sha256').update(tryHash).digest('hex');
 
         const uuid: string = crypto.randomUUID(); 
         global.sessionID = uuid;
 
-        if(correctHashedPassword === hashed){
+        if(correctHash === hashed){
             res.status(StatusCodes.OK).json({
                 message: "User authenticated",
                 sessionID: sessionID
