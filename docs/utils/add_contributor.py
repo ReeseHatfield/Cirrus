f7aee6324a332f2d88fa29d03ef4a74d18ca51b1
Reese Hatfield <hatfield.69@wright.edu>
2024-01-08 16:52:53 -0500

    Update add_contributor.py

--- docs/utils/add_contributor.py
+++ docs/utils/add_contributor.py
@@ -2,7 +2,7 @@
 
 import os
 
-FILE_PATH = '../Credits.md'
+FILE_PATH = 'docs/Credits.md'
 PR_AUTHOR = os.getenv('PR_AUTHOR')
 
 
@@ -12,8 +12,6 @@ PR_AUTHOR = os.getenv('PR_AUTHOR')
 def add_contributor():
     write_name_to_file(PR_AUTHOR)
 
-
-#New test
 
 
 
