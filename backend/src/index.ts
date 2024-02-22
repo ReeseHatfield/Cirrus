cc732b71353f563f10504cef46c53e4f2da5ee4a
ReeseHatfield <hatfield.69@wright.edu>
2024-02-22 11:38:07 -0500

    Refactor index.ts
    Update imports to ES6, Move use() to config, simplify sessionID management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,49 +1,55 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import fileSystemRoutes from './routes/fileSystemRoutes';
 import cron from 'node-cron';
-import fs from 'fs';
+import cors from 'cors';
 
-const cors = require('cors');
-
+// Constants
 const port = process.env.PORT || 3001;
 const frontEndPoint: string = "http://localhost:5173";
 
+// Create Express Instance
 let app: Application = express();
 
-declare global {
-  var sessionID: string;
-}
 
-const resetSessionID = () => {
-  global.sessionID = "";
-}
-
-global.sessionID = ""; //set session id to be empty
+// Create sesssion ID on auth and reset every 30 minutes
+declare global { var sessionID: string; }
+const resetSessionID = () => { global.sessionID = ""; }
+global.sessionID = "";
 cron.schedule('*/30 * * * *', resetSessionID);
 
+// Configure and start Server
 app = config(app);
-app.use("/api", fileSystemRoutes); // use routes from routes/fileSystemRoutes
-
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-//middleware config
+
+
+/**
+ * `config`:
+ * Configures the application to use all required middleware
+ * - Cors (see issue #54)
+ * - Express
+ * - All /api routes from /routes/fileSystemRoutes
+ * @param {Application} app ExpressJS Application Instance to configure
+ * @returns Configured Application.
+ */
 function config(app: Application): Application {
   app.use(cors({
     origin: frontEndPoint,
     
   }));
-
+  
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
+  
   app.use(express.json());
+
+  app.use("/api", fileSystemRoutes); 
 
   return app;
 }
